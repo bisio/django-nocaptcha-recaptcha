d67ed879906f3b3728c2d7a406a164c5cb6cefa9
Andrea Bisognin <andrea.bisognin@gmail.com>
2015-11-24 10:35:44 +0100

    catching the right exception

--- nocaptcha_recaptcha/__init__.py
+++ nocaptcha_recaptcha/__init__.py
@@ -2,7 +2,7 @@ import pkg_resources
 
 try:
     __version__ = pkg_resources.require("django-nocaptcha-recaptcha")[0].version
-except DistributionNotFound:
+except pkg_resources.DistributionNotFound:
     __version__ = '0.0.18-via-buildout'
 
 from .fields import NoReCaptchaField
